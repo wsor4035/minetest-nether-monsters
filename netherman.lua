c48dde8c4c324fdfcded1959e6c2ae7a0151c702
unknown <24964441+wsor4035@users.noreply.github.com>
2021-09-28 23:13:05 -0400

    add missing damage function

--- netherman.lua
+++ netherman.lua
@@ -25,6 +25,27 @@ local effect = function(pos, amount, texture, min_size, max_size, radius, gravit
 	})
 end
 
+--termos MIT
+function node_dps_dmg(self)
+	local pos = self.object:get_pos()
+	local box = self.object:get_properties().collisionbox
+	local pos1 = {x = pos.x + box[1], y = pos.y + box[2], z = pos.z + box[3]}
+	local pos2 = {x = pos.x + box[4], y = pos.y + box[5], z = pos.z + box[6]}
+	local nodes_overlap = mobkit.get_nodes_in_area(pos1, pos2)
+	local total_damage = 0
+
+	for node_def, _ in pairs(nodes_overlap) do
+		local dps = node_def.damage_per_second
+		if dps then
+			total_damage = math.max(total_damage, dps)
+		end
+	end
+
+	if total_damage ~= 0 then
+		mobkit.hurt(self, total_damage)
+	end
+end
+
 --death
 function nm_hq_die(self)
 	local timer = 1.5
@@ -49,7 +70,7 @@ function nm_hq_die(self)
 end
 
 function nm_brain(self)
-    --if mobkit.timer(self,1) then fl_wildlife.node_dps_dmg(self) end --if in nodes with damage take damage
+    if mobkit.timer(self,1) then node_dps_dmg(self) end
     mobkit.vitals(self)
 
     if self.hp <= 0 then --kill self if 0 hp
